3f8767eac8c31944b66152395e761f5b725df92f
menomik1011 <39692905+meno1011@users.noreply.github.com>
2022-09-05 16:58:57 +0900

    다이나믹 url에 StaticProps와 getStaticPath를 설정하여 build후 생성되는 html확인 및 .next폴더 확인

---
+++ pages/detail/[id].js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Loader } from "semantic-ui-react";
+import Item from "../../src/component/Item";
+
+const Post = ({ item, name }) => {
+  return (
+    <>
+      {item && (
+        <>
+          <Head>
+            <title>{item.name}</title>
+            <meta name="description" content={item.description}></meta>
+          </Head>
+          {name} 환경입니다.
+          <Item item={item} />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Post;
+
+// Generates `/posts/1` and `/posts/2`
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: "740" } },
+      { params: { id: "730" } },
+      { params: { id: "729" } }
+    ],
+    fallback: false, // can also be true or 'blocking'
+  };
+}
+
+// getServerSideProps는 browser 환경이 아닌 node js 환경이다.
+export async function getStaticProps(context) {
+  const id = context.params.id;
+  const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const res = await axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      item: data,
+      name: process.env.name,
+    },
+  };
+}
--- pages/index.js
+++ pages/index.js
@@ -5,50 +5,36 @@ import { Divider, Header, Loader } from "semantic-ui-react";
 import ItemList from "../src/component/ItemList";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-  function getDate() {
-    axios.get(API_URL).then((res) => {
-      console.log(res.data);
-      setList(res.data);
-      // api 호출이 끝나고 로딩을 false로 바꿔줌
-      setIsLoading(false);
-    });
-  }
-  useEffect(() => {
-    getDate();
-  }, []);
+export default function Home({ list }) {
   return (
     <div>
       <Head>
         <title>Home | 테스트</title>
         <meta name="description" content="nextjs test 홈입니다."></meta>
       </Head>
-      {isLoading && (
-        <div style={{ padding: "300px 0" }}>
-          <Loader inline="centered" active>
-            Loading
-          </Loader>
-        </div>
-      )}
-      {!isLoading && (
-        <>
-          <Header as="h3" style={{ paddingTop: 40 }}>
-            베스트 상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(0, 9)} />
-          <Header as="h3" style={{ paddingTop: 40 }}>
-            신상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(9)} />
-        </>
-      )}
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        베스트 상품
+      </Header>
+      <Divider />
+      <ItemList list={list.slice(0, 9)} />
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        신상품
+      </Header>
+      <Divider />
+      <ItemList list={list.slice(9)} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const apiUrl = process.env.apiUrl;
+  const res = await axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      list: data,
+      name: process.env.name,
+    },
+  };
 }
